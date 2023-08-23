a4ffbc37b9aa652c493348cdcd9f803f5dc993bd
davc1980 <davc1980@gmail.com>
2023-08-23 16:55:30 -0700

    cambios iniciales webpage

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-import express from "express";
+const express = require("express");
+const path = require("path");
 
-import pg from "pg";
 const app = express();
+
+app.use("/views", express.static("views"));
+app.use("/css", express.static("node_modules/bootstrap/dist/css"));
+app.use("/js", express.static("node_modules/bootstrap/dist/js"));
+app.use("/js", express.static("node_modules/jquery/dist"));
+
 app.get("/", (req, res) => {
-  res.send("Render Example");
+  res.sendFile("index.html");
+});
+app.get("/contact", (req, res) => {
+  res.send("Contact");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
 app.listen(3000);
